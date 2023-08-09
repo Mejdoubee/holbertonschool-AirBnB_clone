cb5f79eb7c04e0aa372068220a467d0d11af171b
Mejdoubee <6235@holbertonstudents.com>
2023-08-09 03:17:50 +0100

    Console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        '''Quit command to exit the program'''
+        return True
+
+    def do_EOF(self, args):
+        '''EOF command to exit the program'''
+        return True
+
+    def emptyline(self):
+        '''An empty line + ENTER shouldn’t execute anything'''
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
