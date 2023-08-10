5e2b4e99486c6ebfedfab14d6486b86fcf81d1e4
Mejdoubee <6235@holbertonstudents.com>
2023-08-10 17:52:38 +0100

    doc

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,25 +15,31 @@ class TestFileStorage(unittest.TestCase):
         self.file_path = "file.json"
 
     def test_file_path(self):
+        '''test_file_path'''
         self.assertEqual(self.storage._FileStorage__file_path, "file.json")
 
     def test_objects(self):
+        '''test_objects'''
         self.assertIsInstance(self.storage._FileStorage__objects, dict)
 
     def test_all(self):
+        '''test_all'''
         all_objects = self.storage.all()
         self.assertIsInstance(all_objects, dict)
 
     def test_new(self):
+        '''test_new'''
         key = f"{self.base_model.__class__.__name__}.{self.base_model.id}"
         self.storage.new(self.base_model)
         self.assertIn(key, self.storage.all())
 
     def test_save(self):
+        '''test_save'''
         self.storage.save()
         self.assertTrue(os.path.exists(self.file_path))
 
     def test_reload(self):
+        '''test_reload'''
         self.storage.save()
         os.remove(self.file_path)
         self.assertFalse(os.path.exists(self.file_path))
@@ -43,12 +49,14 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsInstance(all_objects, dict)
 
     def tearDown(self):
+        '''tearDown'''
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
 
     def test_new_with_different_objects(self):
         '''Test that new method can handle different types of objects'''
         class DummyClass:
+            '''DummyClass'''
             pass
 
         dummy_obj = DummyClass()
@@ -66,15 +74,13 @@ class TestFileStorage(unittest.TestCase):
         '''Test that reload method handles nonexistent file'''
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
-        # Should not raise an exception
-        self.storage.reload()  # Fixed here
+        self.storage.reload()
 
     def test_reload_with_corrupted_json(self):
         '''Test that reload method handles corrupted JSON file'''
         with open(self.file_path, 'w') as file:
             file.write("This is not valid JSON content")
-        # Should not raise an exception
-        self.storage.reload()  # Fixed here
+        self.storage.reload()
 
 
 if __name__ == "__main__":
