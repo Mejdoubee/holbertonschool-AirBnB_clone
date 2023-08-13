434a9d1e652da424c435c4cc89809b18dadbe721
Mejdoubee <6235@holbertonstudents.com>
2023-08-13 03:23:34 +0100

    More classes

--- models/city.py
+++ models/city.py
@@ -5,7 +5,7 @@ Model that defines State class
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class City(BaseModel):
     '''State class that inherits from BaseModel'''
     state_id = ""
     name = ""
