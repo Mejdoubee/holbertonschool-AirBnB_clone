a4518d18aaf71801368973f3c215e80bd4a16c52
Mejdoubee <6235@holbertonstudents.com>
2023-08-13 04:07:53 +0100

    User test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,47 +2,52 @@
 import unittest
 from models.user import User
 from models.base_model import BaseModel
-import datetime
+from datetime import datetime
 
 class TestUser(unittest.TestCase):
-    
+
     def setUp(self):
-        self.user = User()
-        self.user.first_name = "John"
-        self.user.last_name = "Doe"
-        self.user.email = "johndoe@example.com"
-        self.user.password = "password123"
+        '''
+        SetUp testing User
+        '''
+        self.my_user = User()
+        self.my_user.email = "user@example.com"
+        self.my_user.password = "password123"
+        self.my_user.first_name = "John"
+        self.my_user.last_name = "Doe"
 
-    def test_instance(self):
-        self.assertIsInstance(self.user, User)
-        self.assertIsInstance(self.user, BaseModel)
+    def test_initialization(self):
+        self.assertTrue(isinstance(self.my_user, BaseModel))
+        self.assertTrue(hasattr(self.my_user, 'id'))
+        self.assertTrue(hasattr(self.my_user, 'created_at'))
+        self.assertTrue(hasattr(self.my_user, 'updated_at'))
+        self.assertEqual(type(self.my_user.id), str)
+        self.assertEqual(type(self.my_user.created_at), datetime)
 
-    def test_attributes(self):
-        self.assertTrue(hasattr(self.user, 'first_name'))
-        self.assertTrue(hasattr(self.user, 'last_name'))
-        self.assertTrue(hasattr(self.user, 'email'))
-        self.assertTrue(hasattr(self.user, 'password'))
-        self.assertEqual(self.user.first_name, 'John')
-        self.assertEqual(self.user.last_name, 'Doe')
-        self.assertEqual(self.user.email, 'johndoe@example.com')
-        self.assertEqual(self.user.password, 'password123')
+    def test_string_representation(self):
+        user_str = str(self.my_user)
+        self.assertIn(self.my_user.__class__.__name__, user_str)
+        self.assertIn(self.my_user.id, user_str)
+        self.assertIn(str(self.my_user.__dict__), user_str)
 
-    def test_id_generation(self):
-        user1 = User()
-        user2 = User()
-        self.assertNotEqual(user1.id, user2.id)
+    def test_save_method(self):
+        updated_at = self.my_user.updated_at
+        self.my_user.save()
+        self.assertNotEqual(updated_at, self.my_user.updated_at)
 
-    def test_datetime_attributes(self):
-        self.assertIsInstance(self.user.created_at, datetime.datetime)
-        self.assertIsInstance(self.user.updated_at, datetime.datetime)
-
-    def test_to_dict(self):
-        user_dict = self.user.to_dict()
-        self.assertEqual(user_dict['first_name'], 'John')
-        self.assertEqual(user_dict['last_name'], 'Doe')
+    def test_to_dict_method(self):
+        user_dict = self.my_user.to_dict()
+        self.assertIsInstance(user_dict, dict)
         self.assertEqual(user_dict['__class__'], 'User')
-        self.assertIsInstance(user_dict['created_at'], str)
-        self.assertIsInstance(user_dict['updated_at'], str)
+        self.assertIn('id', user_dict)
+        self.assertIn('created_at', user_dict)
+        self.assertIn('updated_at', user_dict)
+
+    def test_custom_attributes(self):
+        self.assertEqual(self.my_user.email, "user@example.com")
+        self.assertEqual(self.my_user.password, "password123")
+        self.assertEqual(self.my_user.first_name, "John")
+        self.assertEqual(self.my_user.last_name, "Doe")
 
 if __name__ == '__main__':
     unittest.main()
